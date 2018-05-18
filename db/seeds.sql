d70d5063ebbf0fc3f6a8b94147205c0e1c0f8937
Mary Quint <mary.k.quint@gmail.com>
2018-05-17 22:32:09 -0500

    added css

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,7 @@
-INSERT INTO burgers (burger_name) VALUES ("California Burger");
-INSERT INTO burgers (burger_name) VALUES ("Avocado Turkey Burger");
-INSERT INTO burgers (burger_name) VALUES ("Cowboy Burger");
+INSERT INTO burgers (burger_name) VALUES ("If Looks Could Kale Burger");
+INSERT INTO burgers (burger_name) VALUES ("I've Created a Muenster Burger");
+INSERT INTO burgers (burger_name) VALUES ("Only the Provolonely Burger");
+INSERT INTO burgers (burger_name) VALUES ("Use It or Bleus It Burger (comes with bleu cheese)");
+INSERT INTO burgers (burger_name) VALUES ("Olive and Let Die Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Fig-eta Bout It Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Final Kraut Down Burger (comes with sauerkraut)");
