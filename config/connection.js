f958074b69d3c9280bf6218e9c03cbf1e690d403
Mary Quint <mary.k.quint@gmail.com>
2018-05-19 13:17:59 -0500

    setting up JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgersDB"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgersDB"
+  });
+
+}
 
 // Make connection.
 connection.connect(function(err) {
